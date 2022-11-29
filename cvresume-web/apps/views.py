64f39c8d1a4d9ab0da030e5dab67ba8718efadf9
Tryd0g0lik <work80@mail.ru>
2022-11-29 18:12:36 +0700

    The path (url ) has been changed of a template

--- cvresume-web/apps/views.py
+++ cvresume-web/apps/views.py
@@ -1,3 +1,18 @@
+from pprint import pprint
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def indexPage(request):
+	context = {}
+
+	# for dict_row in request.__dict__ : pprint(f"request: {dict_row}")
+	pprint(f"request: {request.path }")
+	if request.method == 'GET'\
+	and request.path == '/':
+		return render(
+		request=request,
+		template_name='apps/index.html',
+		context=context,
+	)
